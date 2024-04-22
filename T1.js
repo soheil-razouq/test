9d6fe4fc7c9fb363a6c454f65822707cec9b6bd4
soheil-razouq <126954810+soheil-razouq@users.noreply.github.com>
2024-04-22 18:02:05 +0100

    Update T1.js

--- T1.js
+++ T1.js
@@ -3,15 +3,7 @@ export default function T1(){
     return(
         <>
             T1
-            T2
-            T3hhhgc
-                eveheheh.2hzh2g
-                    b3b3h
-            fudjdjen
-                jdjdndnf
-                    fndnd
-                        sms do do qfzertdfgfgdf
-           
+            T2           
         </>
     )
 }
